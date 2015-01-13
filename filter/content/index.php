a5545bff7e3d9b8dafd6fd2e95129cb0b39aaac5
ershov-ilya <ershov.ilya@gmail.com>
2015-01-13 16:07:20 +0300

    filter api url

--- filter/content/index.php
+++ filter/content/index.php
@@ -8,6 +8,19 @@
  */
 
 header('Content-Type: text/plain; charset=utf-8');
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
-print "parsed:\n";
-print $_POST['content'];
+include('../../lib/simplehtmldom_1_5/simple_html_dom.php');
+$res='';
+
+$html = new simple_html_dom();
+$html->load($_POST['content']);
+
+if(empty($res)) $res=$html->find('#contentTableOuter>tr>td',0)->innertext;
+//if(empty($res)) $res=$html->find('#contentTableOuter>tr>td #contentTableOuter>tr>td',0)->innertext;
+//if(empty($res)) $res=$html->find('body',0)->innertext;
+//if(empty($res)) $res=$html;
+
+if(empty($res)) print "Not match";
+else print $res;
