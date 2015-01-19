db2c3c3992f5e8af5133611872718c9be3cd1e17
ershov-ilya <ershov.ilya@gmail.com>
2015-01-19 11:50:59 +0300

    shell_exec example

---
+++ test/console.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: ershov-ilya
+ * Website: http://ershov.pw/
+ * Date: 19.01.2015
+ * Time: 12:47
+ */
+
+print "Echo string\n";
+return "Return string";
---
+++ test/shell_exec.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: ershov-ilya
+ * Website: http://ershov.pw/
+ * Date: 19.01.2015
+ * Time: 12:49
+ */
+
+$res = shell_exec('php console.php');
+print $res;
