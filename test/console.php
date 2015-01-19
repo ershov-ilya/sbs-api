dab30ea8407013bae6f05c562482c4f27a6c49e9
ershov-ilya <ershov.ilya@gmail.com>
2015-01-19 11:57:37 +0300

    php

--- test/console.php
+++ test/console.php
@@ -8,4 +8,5 @@
  */
 
 print "Echo string\n";
+if(!empty($argv)) print_r($argv);
 return "Return string";
--- test/shell_exec.php
+++ test/shell_exec.php
@@ -7,5 +7,10 @@
  * Time: 12:49
  */
 
-$res = shell_exec('php console.php');
+header('Content-Type: text/plain; charset=utf-8');
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+$res = shell_exec('php console.php console_param1 "comsole param 2"');
 print $res;
