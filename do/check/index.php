1b62dfa8b7b2124b97d3f0d2c765833394044484
ershov-ilya <ershov.ilya@gmail.com>
2015-02-05 10:31:02 +0300

    Исправлен адрес промежуточного файла

--- do/check/index.php
+++ do/check/index.php
@@ -12,6 +12,7 @@ header('Content-Type: text/plain; charset=utf-8');
 if(isset($_GET['filter'])) define('FILTER', true);
 else  define('FILTER', false);
 defined('DEBUG') or define('DEBUG', true);
+$key='sbs';
 
 require('../../config/core.config.php');
 
@@ -25,13 +26,13 @@ try {
     else
     {
         $data=array();
-        $data['last_date'] = '2015-01-13 09:40:39';
+        $data['last_date'] = '2015-01-15 15:57:12';
     }
     $last_date=strtotime($data['last_date']);
         //strtotime
 
 // ЧТЕНИЕ текущего состояния info кэша
-    $file_content = file_get_contents(API_ROOT_PATH.'/emarsys/email/get-ids/cache.dat');
+    $file_content = file_get_contents(API_ROOT_PATH."/emarsys/email/get-ids/$key-cache.dat");
     $obj_list = unserialize($file_content);
 
     $sorted = array();
@@ -39,38 +40,32 @@ try {
         $sorted[]=$value;
     }
 
+    // Сортировка по дате
     usort($sorted, function($objA, $objB)
     {
         $a=strtotime($objA->created);
         $b=strtotime($objB->created);
         if ($a == $b)
         {
-            echo "a ($a) is same priority as b ($b), keeping the same\n";
+//            echo "a ($a) is same priority as b ($b), keeping the same\n";
             return 0;
         }
         else if ($a > $b)
         {
-            echo "a ($a) is higher priority than b ($b), moving b down array\n";
+//            echo "a ($a) is higher priority than b ($b), moving b down array\n";
             return 1;
         }
         else {
-            echo "b ($b) is higher priority than a ($a), moving b up array\n";
+//            echo "b ($b) is higher priority than a ($a), moving b up array\n";
             return -1;
         }
     });
-
-    print_r($sorted);
-
-//    $test = array();
-//    $test[]=$obj_list[0];
-//    print_r($test);
-    exit(0);
 
     $i = 0;
     $result=array();
     $result['array']=array();
     if(FILTER){
-        foreach ($obj_list->data as $el) {
+        foreach ($sorted as $el) {
             if(strtotime($el->created)>$last_date){
                 print $i . ": ";
                 print $el->id . "\t: " . $el->created;
@@ -80,7 +75,7 @@ try {
             $i++;
         }
     }else{
-        foreach ($obj_list->data as $el) {
+        foreach ($sorted as $el) {
             print $i . ": ";
             print $el->id . "\t: " . $el->created;
             if(strtotime($el->created)>$last_date){
