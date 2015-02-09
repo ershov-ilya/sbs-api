9a02231781414e970885a75dbe0435f20acb8754
ershov-ilya <ershov.ilya@gmail.com>
2015-02-09 16:22:09 +0300

    robokassa Примеры

--- payments/robokassa/backurls/result-url.php
+++ payments/robokassa/backurls/result-url.php
@@ -7,7 +7,20 @@
  * Time: 16:52
  */
 
-$json = json_encode($_POST);
-$content = $json."\n\n";
+header('Content-Type: text/plain; charset=utf-8');
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+defined('DEBUG') or define('DEBUG', true);
 
-file_put_contents('log.txt', $content);
+$json = json_encode($_POST);
+
+$curtime=time()-3600;
+$data = date("Y-m-d H:i:s", $curtime);
+$content = $data.' json:'.$json."\n\n";
+
+file_put_contents('log.txt', $content, FILE_APPEND);
+$answer = array(
+    'status'    => 'OK'
+);
+
+print (json_encode($answer));
--- payments/robokassa/do/test.request.php
+++ payments/robokassa/do/test.request.php
@@ -7,5 +7,35 @@
  * Time: 16:57
  */
 
-require_once('../../../lib/curl.php');
+header('Content-Type: text/html; charset=windows-1251');
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+defined('DEBUG') or define('DEBUG', true);
 
+function CURL($url, $data){
+    $ch = curl_init($url);
+
+    //curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    curl_setopt($ch, CURLOPT_POST, true);
+
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Следовать по редиректам
+
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // For Windows machines... some windows misses some CACERT.
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    $output = curl_exec($ch);
+    return  $output;
+}
+
+$data=array(
+    'MrchLogin' => 'SBSEDU',
+    'nOutSum'   => '1000',
+    'nInvId'    => '0',
+    'sInvDesc'  => 'Test',
+    'sIncCurrLabel' => 'RUR',
+    'sCulture'  => 'ru'
+);
+
+$answer = CURL('http://test.robokassa.ru/Index.aspx', $data);
+print $answer;
