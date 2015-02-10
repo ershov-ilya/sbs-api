8f55f74757939043c148e278ae00a82c90bc51e6
ershov-ilya <ershov.ilya@gmail.com>
2015-02-10 10:27:09 +0300

    payments.config.php

---
+++ config/sample.payments.config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: ershov-ilya
+ * Website: http://ershov.pw/
+ * Date: 10.02.2015
+ * Time: 11:25
+ */
+
+$robokassa = array(
+    'mrh_pass1' => "************",
+    'mrh_pass2' => "************"
+);
