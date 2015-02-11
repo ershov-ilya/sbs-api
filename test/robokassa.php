c1906970b0ccbbb589e2d88eabcdebe4e56a9114
ershov-ilya <ershov.ilya@gmail.com>
2015-02-11 12:55:28 +0300

    Database::updateOne()

--- test/robokassa.php
+++ test/robokassa.php
@@ -16,30 +16,33 @@ require_once('../core/config/core.config.php');
 require_once(API_ROOT_PATH.'/core/class/payments/robokassa/robokassa.class.php');
 require_once(API_ROOT_PATH.'/core/config/payments.config.php');
 
+// Get order data
+$order = array();
+$order['OutSum']    = 0;
+$order['Desc']      = "Robokassa testing";
+
+
+// Database save row, and get this row ID
 require_once('../core/class/database/database.class.php');
 require_once('../core/config/pdo.config.php');
 $db = new Database($pdoconfig_lander);
-
-$order = array(
-    'OutSum'  => 200.50,
-    'Desc'    => "Robokassa testing",
-);
 $InvId = $db->putOne('payments', $order);
 
-print $InvId."\n";
-exit(1);
-
-$data=array(
-    'OutSum'        => "16.00",
-    'InvId'         => 12,
-    'Desc'          => "Test",
+$robokassa_data = array(
+    'InvId' => $InvId,
+    'Desc'  => "",
     'IncCurrLabel'  => "",
     'Culture'       => "ru",
     'Encoding'      => "utf-8"
 );
-$robokassa = new Robokassa($data, $payments_config['robokassa']);
+
+$robokassa_data = array_merge($robokassa_data, $order);
+
+$robokassa = new Robokassa($robokassa_data, $payments_config['robokassa']);
 print "payURL: ".$robokassa->payURL()."\n";
-print "check CRC2: ".$robokassa->genCRC2()."\n";
+
+$order['SignatureValue'] = $robokassa->genCRC2();
+$db->updateOne('payments', $InvId, $order);
 
 print_r($robokassa->resultArray());
 
