1d635361cb7d4efd767317e9dfb864bf4eceded8
ershov-ilya <ershov.ilya@gmail.com>
2015-03-13 14:50:09 +0300

    Задача do/process: выгрузка в CSV

--- do/process/index.php
+++ do/process/index.php
@@ -8,7 +8,36 @@
  * Date: 13.03.2015
  * Time: 13:37
  */
-header('Content-Type: text/html; charset=utf-8');
+if(isset($_GET['csv']))
+{
+    define('CSV', true);
+}
+elseif(isset($_GET['plain']))
+{
+    define('PLAIN', true);
+}
+else
+{
+    define('HTML', true);
+}
+
+defined('CSV') or define('CSV', false);
+defined('HTML') or define('HTML', false);
+defined('PLAIN') or define('PLAIN', false);
+
+if(CSV)
+{
+    header('Content-type: text/csv');
+    header('Content-disposition: attachment;filename=output.csv');
+}
+elseif(HTML)
+{
+    header('Content-Type: text/html; charset=utf-8');
+}
+else
+{
+    header('Content-Type: text/plain; charset=utf-8');
+}
 
 // config
 require_once('../../core/config/core.config.php');
@@ -21,12 +50,23 @@ $data=file(BASE_URL.'/do/get-email-ids/'); // Параметр ?re для сбр
 $index=array();
 foreach($data as $str)
 {
-    $arr = explode(';', $str, 3);
+    $str=preg_replace("/[\n\r]+/",'',$str);
+    $arr = explode(';', $str);
     $index[$arr[0]] = $arr;
+
+//   print_r($arr);
+//    print $str."\n";
 }
+//exit(0);
 
 // Get data
 $content=file('input.csv');
+
+$headers=$content[0];
+$headers=preg_replace('/.*ID/','ID',$headers);
+$headers=preg_replace('/\r\n/','',$headers);
+$headers.=";Дата;Тема\n";
+
 unset($content[0]);
 
 // Вычисления
@@ -34,9 +74,24 @@ foreach($content as $str)
 {
     $str=preg_replace('/\r\n/','',$str);
     $arr = explode(';', $str);
-    $output.=$str.';'.$index[$arr[0]][2];
+    $id=$arr[0];
+
+//    $date=$index[$id][2];
+//    $theme=preg_replace('/\r\n/','',$index[$id][3]);
+//    $output.=$id.";".$date.";".$theme;
+    $output.=$id.";".$arr[1].";".$index[$id][2].";".$index[$id][3]."\n";
+}
+
+$output=$headers.$output;
+
+if(CSV) {
+    $output=mb_convert_encoding($output, "windows-1251", "utf-8");
+}
+
+if(CSV || PLAIN) {
+    print $output;
 }
 
 // Вывод шаблона
-include(TEMPLATE_PATH.'/base.tpl.php');
+if(HTML) include(TEMPLATE_PATH.'/base.tpl.php');
 
