c4b62f3decbb80ad8add45884b26fe361a23df52
ershov-ilya <ershov.ilya@gmail.com>
2015-03-13 12:39:01 +0300

    do/get-info

---
+++ do/get-info/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author:   ershov-ilya
+ * GitHub:   https://github.com/ershov-ilya/
+ * About me: http://about.me/ershov.ilya (EN)
+ * Website:  http://ershov.pw/ (RU)
+ * Date: 13.03.2015
+ * Time: 13:37
+ */
+
+$content=file('input.csv');
+
+print_r($content[0]);
