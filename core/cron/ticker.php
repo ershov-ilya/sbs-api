a76a75053bd42b2802a953c1bf69ccc58b8c3106
ershov-ilya <ershov.ilya@gmail.com>
2015-07-14 11:51:08 +0300

    cron ticker, abs path

--- core/cron/ticker.php
+++ core/cron/ticker.php
@@ -11,10 +11,24 @@
 if(isset($_GET['t'])) define('DEBUG',true);
 else  define('DEBUG',false);
 
-$file='cron.log';
-$date=date("d.m.Y G:i:s");
-$text="tick $date";
-if(isset($_SERVER)) $text.=" SERVER;\n";
-if(isset($argv)) $text.=" argv;\n";
-$text.="\n";
-file_put_contents($file, $text, FILE_APPEND);
+header('Content-Type: text/plain; charset=utf-8');
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+define('API_ROOT',dirname(dirname(dirname($_SERVER['SCRIPT_FILENAME']))));
+require_once(API_ROOT.'/core/config/core.config.php');
+
+if(isset($argv)){
+    $file=API_ROOT.'/core/cron/cron.log';
+    $date=date("d.m.Y G:i:s");
+    $text="tick $date";
+//if(isset($_SERVER)) $text.=" SERVER;\n";
+    if(isset($argv)) $text.=" console\n";
+    $text.="\n";
+    $text.=$_SERVER['DOCUMENT_ROOT']."\n";
+    $text.=$_SERVER['SCRIPT_FILENAME']."\n";
+    file_put_contents($file, $text, FILE_APPEND);
+    if(DEBUG) print '$argv isset!'.PHP_EOL;
+    exit(0);
+}
+
