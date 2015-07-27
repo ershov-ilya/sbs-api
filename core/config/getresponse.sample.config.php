ef2cb9cbafc778c41593288498e4af5d5d599abe
ershov-ilya <ershov.ilya@gmail.com>
2015-07-27 17:21:25 +0300

    getResponse API php коннектор

---
+++ core/config/getresponse.sample.config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author:   ershov-ilya
+ * GitHub:   https://github.com/ershov-ilya/
+ * About me: http://about.me/ershov.ilya (EN)
+ * Website:  http://ershov.pw/ (RU)
+ * Date: 27.07.2015
+ * Time: 17:45
+ */
+
+$getresponse_config=array(
+    'url' => 'http://api2.getresponse.com',
+    'key' => 'API_KEY'
+);
