e7c438a874ec268abbcdc22af4a35e6d4b8d6944
ershov-ilya <ershov.ilya@gmail.com>
2015-07-29 12:51:05 +0300

    insert in modx

--- core/cron/getresponse/getresponse_insert_in_modx.php
+++ core/cron/getresponse/getresponse_insert_in_modx.php
@@ -13,7 +13,7 @@ if(isset($_GET['t'])) define('DEBUG',true);
 else  define('DEBUG',false);
 
 header('Content-Type: text/plain; charset=utf-8');
-error_reporting(E_ALL);
+error_reporting(E_ERROR | E_WARNING);
 ini_set("display_errors", 1);
 
 define('API_ROOT',dirname(dirname(dirname(dirname($_SERVER['SCRIPT_FILENAME'])))));
@@ -23,29 +23,67 @@ require_once(API_CORE_PATH.'/class/format/format.class.php');
 require_once(API_CORE_PATH.'/class/database/database.class.php');
 require_once(API_ROOT_PATH.'/getresponse/get_message_contents.php');
 
-//$messages=get_messages();
-//print_r($messages);
 
 $db=new Database($pdoconfig);
-$message=$db->getOne('getresponse_tasks','content','state');
-if(empty($message)) exit(0); // Точка останова, если делать ничего не надо
-print_r($message);
+$task=$db->getOne('getresponse_tasks','parsed','state');
+if(empty($task)) exit(0); // Точка останова, если делать ничего не надо
+print_r($task);
 
+exit(0);
+// Подключаем
+define('MODX_API_MODE', true);
+require('../../../index.php');
 
-/*
- * $content=get_message_contents($row['message_id']);
-//print $content;
+/* @var modX $modx */
 
-$db->updateOne('getresponse_tasks',$row['id'],array(
-    'state'=>'content',
-    'content' => $content
-));
+// Включаем обработку ошибок
+$modx->getService('error','error.modError');
+$modx->setLogLevel(modX::LOG_LEVEL_FATAL);
+$modx->setLogTarget(XPDO_CLI_MODE ? 'ECHO' : 'HTML');
+$modx->error->message = null; // Обнуляем переменную
 
-$errors=$db->errors();
-if(!empty($errors)){
-    $db->sayError();
-}
+// Логинимся в админку
+//$response = $modx->runProcessor('security/login', array('username' => '***', 'password' => '******'));
+//print_r($response->response);
+//if ($response->isError()) {
+//    $modx->log(modX::LOG_LEVEL_ERROR, $response->getMessage());
+//    return;
+//}
+//$modx->initialize('mgr');
 
-//if(DEBUG) print Format::parse($response, 'php');
-//else  print Format::parse($response, 'json');
-*/
+$config=array(
+    'parent' => '973',
+    'template' => '27',
+    'published' => '1'
+);
+
+$config=array_merge($config,$_REQUEST);
+
+print_r($config);
+
+// Создаем ресурс
+//$response = $modx->runProcessor('resource/create', $config);
+//print_r($response->response);
+
+//if ($response->isError()) {
+//    $modx->log(modX::LOG_LEVEL_ERROR, $response->getMessage());
+//    return;
+//}
+//else {
+//    print_r($response->response);
+//}
+
+/* @var modResource $resource */
+$resource=$modx->newObject('modResource', $config);
+
+print 'New doc: ';
+print "\n";
+
+print $resource->get('pagetitle');
+print "\n";
+print "ID: ";
+print $resource->get('id');
+print "\n";
+
+// TODO: $doc->save();
+var_dump($resource->save());
