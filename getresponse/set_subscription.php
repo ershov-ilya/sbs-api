2b784fcb137973609e3dd30294228547d19fba46
ershov-ilya <ershov.ilya@gmail.com>
2015-07-31 11:26:37 +0300

    translate_field

--- getresponse/set_subscription.php
+++ getresponse/set_subscription.php
@@ -12,7 +12,7 @@
 header('Content-Type: text/plain; charset=utf-8');
 //error_reporting(E_ALL);
 //ini_set("display_errors", 1);
-//defined('DEBUG') or define('DEBUG', true);
+defined('DEBUG') or define('DEBUG', true);
 
 define('API_ROOT',dirname(dirname($_SERVER['SCRIPT_FILENAME'])));
 require_once(API_ROOT.'/core/config/core.config.php');
@@ -27,9 +27,55 @@ $subscriptions=array(
     'test1'     => 1
 );
 
-set_subscription($user,$subscriptions);
+test($user,$subscriptions);
+//set_subscription($user,$subscriptions);
 
-/* @define API_CORE_PATH /core */
+
+function translate_field($field, $dir='form-id'){
+    $campaigns=array(
+        'free_webinars' =>  'webinar',
+        'knowledge_base'=>  'base',
+        'events'        =>  'open_program'
+    );
+
+    // Узнавать через функцию test / getresponse_get_campaign_id()
+    $campaign_ids=array(
+        'webinar'   =>  'R',
+        'base'      =>  'l',
+        'open_program' =>  'J'
+    );
+
+    switch($dir){
+        case 'form-campaign':
+            return $campaigns[$field];
+        case 'form-id':
+            return $campaign_ids[$campaigns[$field]];
+        default:
+            return $campaigns[$field];
+    }
+    return 'not_found';
+}
+
+
+function test($user, $subscriptions){
+    require_once(API_CORE_PATH.'/config/getresponse.private.config.php');
+    $account=$getresponse_config;
+
+    require_once(API_ROOT_PATH.'/getresponse/jsonRPCClient.php');
+    $rpcClient = new jsonRPCClient($getresponse_config['url']);
+
+    var_dump(
+        translate_field('free_webinars')
+    );
+//    var_dump(getresponse_get_contacts($user, $account, $rpcClient));
+//    var_dump(getresponse_get_contact_ids($user, $account, $rpcClient));
+//    var_dump(getresponse_get_campaign_name('a', $account, $rpcClient));
+//    var_dump(getresponse_get_campaign_id('J', $account, $rpcClient));
+//    var_dump(getresponse_add_contact($user, $account, $rpcClient));
+//    var_dump(getresponse_delete_contact('LHEr', $account, $rpcClient));
+//    var_dump(getresponse_delete_email($user, $account, $rpcClient));
+}
+
 function set_subscription($user, $subscriptions){
     require_once(API_CORE_PATH.'/config/getresponse.private.config.php');
     $account=$getresponse_config;
@@ -37,16 +83,9 @@ function set_subscription($user, $subscriptions){
     require_once(API_ROOT_PATH.'/getresponse/jsonRPCClient.php');
     $rpcClient = new jsonRPCClient($getresponse_config['url']);
 
-    foreach($subscriptions as $subscription){
-        var_dump($subscription);
-    }
-
-    var_dump(getresponse_get_contacts($user, $account, $rpcClient));
-//    var_dump(getresponse_get_contact_ids($user, $account, $rpcClient));
-//    var_dump(getresponse_get_campaign_name('a', $account, $rpcClient));
-//    var_dump(getresponse_add_contact($user, $account, $rpcClient));
-//    var_dump(getresponse_delete_contact('LHEr', $account, $rpcClient));
-//    var_dump(getresponse_delete_email($user, $account, $rpcClient));
+//    foreach($subscriptions as $subscription){
+//        print_r($subscription);
+//    }
 }
 
 
@@ -114,6 +153,26 @@ function getresponse_get_campaign_name($campaign_id, $account, &$rpcClient)
         break;
     }
     return false;
+}
+
+function getresponse_get_campaign_id($campaign_name, $account, &$rpcClient)
+{
+    if(DEBUG) {
+        print "getresponse_get_campaign_id\n";
+        print "campaign_name $campaign_name\n";
+    }
+    $campaigns = $rpcClient->get_campaigns(
+        $account['key'],
+        array(
+            # find by name literally
+            'name' => array(
+            "EQUALS" => $campaign_name
+            )
+        )
+    );
+    if(empty($campaigns)) return false;
+    $campaign_ids = array_keys($campaigns);
+    return $campaign_ids[0];
 }
 
 function getresponse_delete_contact($contact_id, $account, &$rpcClient)
