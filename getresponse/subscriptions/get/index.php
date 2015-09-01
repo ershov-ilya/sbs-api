12f9eebcc7aa8b8248f54b7c373b64e6819224e1
ershov-ilya <ershov.ilya@gmail.com>
2015-09-01 13:44:00 +0300

    getresponse/subscriptions/get done

--- getresponse/subscriptions/get/index.php
+++ getresponse/subscriptions/get/index.php
@@ -16,27 +16,29 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 defined('DEBUG') or define('DEBUG', false);
 $response=array();
+$data=array();
+
 $format='php';
 
-require_once('../../core/config/core.config.php');
+require_once('../../../core/config/core.config.php');
 require_once(API_CORE_PATH.'/class/restful/restful.class.php');
-//require_once(API_ROOT_PATH.'/getresponse/func/get_subscriptions.php');
-//require_once(API_ROOT_PATH.'/getresponse/func/curl_request.php');
 require_once(API_ROOT_PATH.'/getresponse/func/jsonRPCClient.php');
 require_once(API_CORE_PATH.'/config/getresponse.private.config.php');
 
-//define('INCLUSION', true);
-//require_once(API_ROOT_PATH . '/getresponse/func/set_subscription.php');
+define('INCLUSION', true);
+require_once(API_ROOT_PATH . '/getresponse/func/set_subscription.php');
 
-//print_r($_SERVER);
-//die;
 try {
+    // Парсинг параметров
     $rest = new RESTful('subscriptions','email');
     if(empty($rest->data['email'])) throw new Exception('No email passed', 400);
 
+    // Инициализация клиента
     $account=$getresponse_config;
     $rpcClient = new jsonRPCClient($getresponse_config['url']);
 
+    // Вычислительный процесс
+    // Список клиентов
     $contacts = $rpcClient->get_contacts(
         $account['key'],
         array(
@@ -44,17 +46,46 @@ try {
             'email' => array('EQUALS' => $rest->data['email'])
         )
     );
-    print_r($contacts);
-    die;
+//    print_r($contacts);
 
+    $campaigns=array();
+    $campaign_ids=array();
+    foreach($contacts as $contact){
+        $campaign=array(
+            'id'=>$contact['campaign']
+        );
 
-//    $response['data']=get_subscriptions($rest->data);
+        $camp_data = $rpcClient->get_campaign(
+            $account['key'],
+            array(
+                # find by name literally
+                'campaign' => $campaign['id']
+            )
+        );
+        foreach($camp_data as $camp) {
+            $campaign = array_merge($campaign, $camp);
+            break;
+        }
+        $campaign_ids[]=$campaign['id'];
+        $campaigns[]=$campaign;
+    }
+
+//    print_r($campaign_ids);
+//    print_r($campaigns);
+
+    // Переобор кампаний
+    if(empty($campaigns)) throw new Exception('Campaigns not found', 404);
+    foreach($campaigns as $c){
+        $data[$c['id']]=$c['name'];
+        if(isset($c['description'])) $data[$c['id']]=$c['description'];
+    };
 }
 catch(Exception $e){
     $response['message']=$e->getMessage();
     $response['code']=$e->getCode();
 }
 
+$response['data']=$data;
 require_once(API_CORE_PATH.'/class/format/format.class.php');
 print Format::parse($response, $format);
 
